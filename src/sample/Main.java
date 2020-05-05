c6f16422542129c4778c2ff2e5de9d166f73a334
Aleksandar Pantic <47724918+Alekpant@users.noreply.github.com>
2020-05-05 21:03:01 +0200

    novi commit

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World waz up OG");
+        primaryStage.setTitle("Hello World waz up OG alek");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
