af8484fb3f160ce081da7a15f09069aa75cdb63e
Огњен Богићевић <bo160571d@student.etf.bg.ac.rs>
2020-05-06 20:20:21 +0200

    graphics start commit

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World waz up OG alek master");
+        primaryStage.setTitle("OGALEK en/de/cryptor");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
