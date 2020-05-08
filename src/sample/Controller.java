03d6c5a4a83310416e16048614c0fc77a9f663dc
Огњен Богићевић <bo160571d@student.etf.bg.ac.rs>
2020-05-08 20:40:01 +0200

    graphics start commit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,34 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class Controller {
+    @FXML
+    private Button createKey;
+    @FXML
+    private Button finishKey;
+    @FXML
+    private void handleCreateButtonAction(ActionEvent event) throws IOException {
+        Stage stage;
+        Parent root;
+        if(event.getSource() == createKey) {
+            stage = (Stage) createKey.getScene().getWindow();
+            root = FXMLLoader.load(getClass().getResource("keyCreate.fxml"));
+        } else {
+            stage = (Stage) finishKey.getScene().getWindow();
+            root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        }
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,15 +4,21 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+    Stage mainStage;
+    Label somelabel;
+    Button someButton;
+    Scene scene1, scene2;
     @Override
     public void start(Stage primaryStage) throws Exception{
+        mainStage = primaryStage;
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("OGALEK en/de/cryptor");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
 
